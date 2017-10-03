7e95687968bdd221f0aad78a5a8ca83c40f1ebe9
mkem114 <mkem114@aucklanduni.ac.nz>
2017-10-04 08:49:01 +1300

    Basic Outline

---
+++ BadManor/Assets/Scripts/GameManager.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class GameManager : MonoBehaviour
+    {
+        public static GameManager inst = null;
+        private void Awake()
+        {
+            inst = inst ?? this;
+            if (inst != this)
+            {
+                Destroy(gameObject);
+            }
+            DontDestroyOnLoad(gameObject);
+            InitGame();
+        }
+
+        void InitGame()
+        {
+            // Start everything here
+            // Load to main screen
+            // Find save game
+            // Pre-load save game
+            // Load options picked
+            // Get ready to start timing
+        }
+    }
+}
---
+++ BadManor/Assets/Scripts/Items/Item.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.Scripts.Items
+{
+    abstract class Item : MonoBehaviour
+    {
+    }
+}
