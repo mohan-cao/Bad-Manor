519379033b0c9627feba2d8f3ac72f0ea05bea4e
mkem114 <mkem114@aucklanduni.ac.nz>
2017-10-04 09:57:51 +1300

    ScoreManager added

--- BadManor/Assets/Scripts/GameManager.cs
+++ BadManor/Assets/Scripts/GameManager.cs
@@ -8,6 +8,13 @@ namespace Assets.Scripts
 {
     public class GameManager : MonoBehaviour
     {
+        private CharacterManager cm;
+        private ItemManager im;
+        private ScoreManager scorem;
+        private SoundManager soundm;
+        private UIManager uim;
+        private WorldManager wm;
+
         public static GameManager inst = null;
         private void Awake()
         {
@@ -22,12 +29,23 @@ namespace Assets.Scripts
 
         void InitGame()
         {
+            cm = new CharacterManager();
+            im = new ItemManager();
+            scorem = new ScoreManager();
+            soundm = new SoundManager();
+            uim = new UIManager();
+            wm = new WorldManager();
             // Start everything here
             // Load to main screen
             // Find save game
             // Pre-load save game
             // Load options picked
             // Get ready to start timing
+        }
+
+        public long timeSinceStart()
+        {
+            return sm.timeSinceStart();
         }
     }
 }
