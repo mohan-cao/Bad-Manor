e775efe4c5c6c7fb833aa6c5110bd8082cb097ae
Unknown <spt098@aucklanduni.ac.nz>
2017-10-05 20:59:57 +1300

    Approach for Dialogue Conversation

---
+++ BadManor/Assets/Scripts/Characters/DialogueHolder.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DialogueHolder : MonoBehaviour {
+
+	public string dialogue;
+	private DialogueManager dMan;
+	// Use this for initialization
+	void Start () {
+		dMan = FindObjectOfType<DialogueManager> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerStay2D(Collider2D other){
+		if (other.gameObject.name == "Player") {
+			if (Input.GetKeyUp(KeyCode.Space)) {
+				dMan.ShowBox (dialogue);
+			
+			}
+		
+		}
+		
+	}
+
+}
---
+++ BadManor/Assets/Scripts/Characters/DialogueManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class DialogueManager : MonoBehaviour {
+
+	public GameObject dBox;
+	public Text dText;
+	public bool dialogActive;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (dialogActive && Input.GetKeyDown(KeyCode.Space)) {
+			dBox.SetActive (false);
+			dialogActive = false;
+		}
+	}
+
+	public void ShowBox(string dialogue){
+		dialogActive = true;
+		dBox.SetActive (true);
+		dText.text = dialogue;
+	
+	}
+}
