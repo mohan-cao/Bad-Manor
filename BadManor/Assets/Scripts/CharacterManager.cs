a74d05cdd5876952154685f87ec598dd7de541a2
SCOOBY-SURFACE\andre <andrew-lyall@hotmail.com>
2017-10-05 16:21:04 +1300

    Made some parts of characters.png is transparent; Player now moving - still working on collision

--- BadManor/Assets/Scripts/CharacterManager.cs
+++ BadManor/Assets/Scripts/CharacterManager.cs
@@ -7,7 +7,25 @@ using UnityEngine;
 
 namespace Assets.Scripts
 {
-    class CharacterManager : MonoBehaviour
+	class CharacterManager : MonoBehaviour
     {
+
+		public float moveTime = 0.1f;		//Time to move character in seconds
+
+		private BoxCollider2D collide;		//Instance of BoxCollider2D attached to this character
+		private Rigidbody2D rb2d;			//Instance of RigidBody2D attached to this character
+
+		public virtual void Start()
+		{
+			//Initialise 'collide' with reference to BoxCollider2D
+			collide = GetComponent<BoxCollider2D> ();
+
+			//Initialise 'rb2d' with reference to RigidBody2D
+			rb2d = GetComponent<Rigidbody2D> ();
+		}
+
+		//public bool Move (int xDir, int yDir, 
+
     }
+
 }
--- BadManor/Assets/Scripts/Characters/Player.cs
+++ BadManor/Assets/Scripts/Characters/Player.cs
@@ -1,11 +1,48 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using UnityEngine;
 
 namespace Assets.Scripts.Characters
 {
     class Player : Character
     {
+
+		public float moveTime = 0.1f;
+
+		private Rigidbody2D rb2d;
+		private BoxCollider2D collider;
+
+		void Start()
+		{
+			rb2d = GetComponent<Rigidbody2D> ();
+			collider = GetComponent<BoxCollider2D> ();
+		}
+
+		void Update()
+		{
+			float moveHorizontal = Input.GetAxisRaw ("Horizontal");
+			float moveVertical = Input.GetAxisRaw ("Vertical");
+
+			if (moveVertical != 0 || moveHorizontal != 0) 
+			{
+				Vector3 start = transform.position;
+				Vector3 movement = new Vector3 (moveHorizontal, moveVertical, 0f);
+				Vector3 end = start + (movement * moveTime);
+
+				//collider.enabled = false;
+				//RaycastHit2D hit = Physics2D.Linecast (new Vector2(start.x, start.y), new Vector2(end.x, end.y));
+				//collider.enabled = true;
+
+				//if (hit.transform == null) 
+				//{
+					transform.Translate (movement * moveTime);
+				//}
+
+			}
+		}
+
     }
 }
