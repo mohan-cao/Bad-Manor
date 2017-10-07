f9b75d8546aa46a1cb335f0f405036583879dc88
Terran <terrankroft@gmail.com>
2017-10-07 16:58:06 +1300

    Redesign map to be on one scene

---
+++ BadManor/Assets/Scripts/World/Ground/GroundHall.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Assets.Scripts.World
+{
+    class GroundHall : Room
+    {
+
+    }
+}
---
+++ BadManor/Assets/Scripts/World/WarpPad.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System;
+using UnityEngine.SceneManagement;
+
+namespace Assets.Scripts.World
+{
+    public class WarpPad : MonoBehaviour
+    {
+
+        public int SceneToTeleportTo;
+        public float hardCodeX;
+        public float hardCodeY;
+
+        // Use this for initialization
+        void OnTriggerStay2D(Collider2D other)
+        {
+            if (other.gameObject.tag == "Player")
+            {
+                SceneManager.LoadScene(SceneToTeleportTo);
+                other.transform.position = new Vector3(hardCodeX, hardCodeY);
+            }
+        }
+    }
+}
