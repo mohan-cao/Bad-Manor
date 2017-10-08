84fadb36e50fe1747801f02a2ff8d495b38e1909
TOM-DESKTOP\Andrew <andrew-lyall@hotmail.com>
2017-10-08 19:02:26 +1300

    updates

---
+++ BadManor/Assets/Scripts/Characters/CharacterDialogue/AnviDialogue.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnviDialogue : MonoBehaviour {
+
+    //private string name = "Anvi";
+
+    //private Dic
+
+}
--- BadManor/Assets/Scripts/Characters/DialogueManager.cs
+++ BadManor/Assets/Scripts/Characters/DialogueManager.cs
@@ -1,38 +1,8 @@
 ﻿using UnityEngine;
 using System.Collections;
-using UnityEngine.UI;
 
 public class DialogueManager : MonoBehaviour {
 
-	public GameObject dBox;
-	public Text dText;
+	
 
-	private string[] dialogueLines;
-	private int currentLine;
-	// Use this for initialization
-
-	public void StartConvo() {
-		dialogueLines = new string[] {"This is line one", "This is my second line of dialog", "While am I still talking, this is my third line"};
-		currentLine = -1;
-
-		//dText.text = dialogueLines [currentLine];
-		dBox.SetActive (true);
-	}
-
-	public bool NextLine()
-	{
-		currentLine++;
-		if (currentLine >= dialogueLines.Length) 
-		{
-			return false;
-		}
-		dText.text = dialogueLines [currentLine];
-		return true;
-	}
-
-	public void EndConvo() 
-	{
-		currentLine = 0;
-		dBox.SetActive (false);
-	}
 }
