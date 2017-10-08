3d651242de2ce2bdc2985fdb989d898af63ffbc9
TOM-DESKTOP\Andrew <andrew-lyall@hotmail.com>
2017-10-08 19:47:36 +1300

    Complete Anvi Dialogue class; Template created for others

--- BadManor/Assets/Scripts/Characters/CharacterDialogue/SamDialogue.cs
+++ BadManor/Assets/Scripts/Characters/CharacterDialogue/SamDialogue.cs
@@ -1,15 +1,35 @@
 ﻿using UnityEngine;
 using System.Collections;
+using Assets.Scripts;
+using System.Collections.Generic;
 
 public class SamDialogue : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    private string characterName = "Brange";
+    private Dictionary<GameManager.GameState, string[]> storyLines;
+    private List<string> randomLines = new List<string>();
+
+    private void Start()
+    {
+        storyLines[GameManager.GameState.TUTORIAL_1] = new string[] { "DIALOGUE LINE 1", "CHARACTER WHO SAYS DIALOGUE LINE 1", "DIALOGUE LINE 2", "CHARACTER WHO SAYS DIALOGUE LINE 2" };
+
+        randomLines.Add("I NEED DIALOGUE");
+    }
+
+    public string[] getStoryLines(GameManager.GameState state)
+    {
+        string[] value = null;
+        if (storyLines.TryGetValue(state, out value))
+        {
+            return value;
+        }
+        return null;
+    }
+
+    public string getRandomLine()
+    {
+        int index = (int)Random.Range(0f, randomLines.Count - 1);
+        return randomLines[index];
+    }
+
 }
