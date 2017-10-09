3ee9381bff9766cda0ac99f2dab76367512d8165
mkem114 <mkem114@aucklanduni.ac.nz>
2017-10-10 03:33:49 +1300

    CharacterManager Commenting and Refactoring
    Commented class, methods, lines and fields
    Refactored by extracting conversation management to another method
    Decreased visibility where possible
    Removed useless imports
    Renamed fields to conform with conventions
    Stopped overflowing lines
    Removed useless method stubs

--- BadManor/Assets/Scripts/CharacterManager.cs
+++ BadManor/Assets/Scripts/CharacterManager.cs
@@ -1,31 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using Assets.Scripts.Characters;
-using UnityEngine;
+﻿using UnityEngine;
 
 namespace Assets.Scripts
 {
+	/// <summary>
+	/// CharacterManager manages all characters in the game. It currently has no respoonsibility for the prototype
+	/// but in the future it will manage movement, interaction and dialogue of them. It currently doesn't depend on any
+	/// other class at the moment but it will liekly depend on Character later.</summary>
 	public class CharacterManager : MonoBehaviour
     {
-
-		public float moveTime = 0.1f;		//Time to move character in seconds
-
-		private BoxCollider2D collide;		//Instance of BoxCollider2D attached to this character
-		private Rigidbody2D rb2d;			//Instance of RigidBody2D attached to this character
-
-		public virtual void Start()
-		{
-			//Initialise 'collide' with reference to BoxCollider2D
-			collide = GetComponent<BoxCollider2D> ();
-
-			//Initialise 'rb2d' with reference to RigidBody2D
-			rb2d = GetComponent<Rigidbody2D> ();
-		}
-
-		//public bool Move (int xDir, int yDir, 
-
+	    /// <summary>
+	    /// The time for which characters move in the game per phsyics frame.</summary>
+		public float moveTime = 0.1f;
     }
-
 }
