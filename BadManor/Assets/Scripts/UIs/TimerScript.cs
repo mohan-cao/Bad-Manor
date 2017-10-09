d7086bfaa2abc22742a81f1997a66f8991a1e56a
mkem114 <mkem114@aucklanduni.ac.nz>
2017-10-09 19:20:41 +1300

    Made the timer

---
+++ BadManor/Assets/Scripts/UIs/TimerScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class TimerScript : MonoBehaviour
+{
+
+	public Text counterText;
+
+	public float seconds, minutes;
+	// Use this for initialization
+	void Start ()
+	{
+		counterText = GetComponent<Text>() as Text; 
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		minutes = (int) (Time.time / 60f);
+		seconds= (int)(Time.time % 60f);
+		counterText.text = "Playing for : " + minutes.ToString("00") + ":" + seconds.ToString("00");
+
+	}
+}
