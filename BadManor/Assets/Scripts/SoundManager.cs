f756254177712de400065d1048d33d966472cf3d
mkem114 <mkem114@aucklanduni.ac.nz>
2017-10-10 04:20:06 +1300

    SoundManager Commenting and Refactoring
    Commented class, methods, lines and fields
    Refactored by extracting conversation management to another method
    Decreased visibility where possible
    Removed useless imports
    Renamed fields to conform with conventions
    Stopped overflowing lines
    Removed useless method stubs

--- BadManor/Assets/Scripts/SoundManager.cs
+++ BadManor/Assets/Scripts/SoundManager.cs
@@ -1,12 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using Assets.Scripts.Sounds;
-using UnityEngine;
+﻿using UnityEngine;
 
 namespace Assets.Scripts
 {
+    /// <summary>
+    /// SoundManager will manage sound effects and music but for prototype this does nothing.</summary>
     public class SoundManager : MonoBehaviour
     {
     }
