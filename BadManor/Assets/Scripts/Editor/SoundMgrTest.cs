d818d4966ea1ef70d7446bd6c0c028aacdb28fd5
mohan-cao <mohancao@yahoo.com.au>
2017-10-21 16:49:42 +1300

    SoundManager added

---
+++ BadManor/Assets/Scripts/Editor/SoundMgrTest.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using UnityEditor;
+using NUnit.Framework;
+using Assets.Scripts;
+
+public class SoundMgrTest {
+
+	[Test]
+	public void SoundManagerTest()
+	{
+		//Arrange
+		var gameObject = new GameObject();
+
+		// Init a sound manager
+		SoundManager smgr = gameObject.AddComponent<SoundManager>();
+		smgr.musicSource = new AudioSource ();
+		smgr.ExecAwakeStartUpdate ();
+
+		// Make an audio clip
+		AudioClip a = (AudioClip) EditorGUIUtility.Load("Assets/Resources/piano2.wav");
+
+		//Play audio clip
+		string key = smgr.PlaySound (a);
+		Assert.AreEqual (key, "test");
+	}
+	
+	[Test]
+	public void SoundManagerStopTest()
+	{
+		//Arrange
+		var gameObject = new GameObject();
+
+		// Init a sound manager
+		SoundManager smgr = gameObject.AddComponent<SoundManager>();
+		smgr.musicSource = new AudioSource ();
+		smgr.ExecAwakeStartUpdate ();
+
+		// Make an audio clip
+		AudioClip a = (AudioClip) EditorGUIUtility.Load("Assets/Resources/piano2.wav");
+
+		//Play audio clip
+		string key = smgr.PlaySound (a);
+		Assert.AreEqual (key, "test");
+		//Try stop audio clip
+		bool wasPlaying = smgr.StopAudioIfPlaying ("test");
+		Assert.IsTrue (wasPlaying);
+	}
+
+}
--- BadManor/Assets/Scripts/SoundManager.cs
+++ BadManor/Assets/Scripts/SoundManager.cs
@@ -1,10 +1,81 @@
 ﻿using UnityEngine;
+using System.Collections.Generic;
 
 namespace Assets.Scripts
 {
     /// <summary>
     /// SoundManager will manage sound effects and music but for prototype this does nothing.</summary>
-    public class SoundManager : MonoBehaviour
-    {
-    }
+	public class SoundManager : MonoBehaviour 
+	{
+		private Dictionary<string,AudioSource> efxSource = new Dictionary<string,AudioSource>();                   //Drag a reference to the audio source which will play the sound effects.
+		public AudioSource musicSource;                 //Drag a reference to the audio source which will play the music.
+		public static SoundManager instance = null;     //Allows other scripts to call functions from SoundManager.
+		private static int counter = 0;
+
+		void Awake ()
+		{
+			//Check if there is already an instance of SoundManager
+			if (instance == null)
+				//if not, set it to this.
+				instance = this;
+			//If instance already exists:
+			else if (instance != this)
+				//Destroy this, this enforces our singleton pattern so there can only be one instance of SoundManager.
+				Destroy (gameObject);
+
+			//Set SoundManager to DontDestroyOnLoad so that it won't be destroyed when reloading our scene.
+			DontDestroyOnLoad (gameObject);
+		}
+
+
+		/**
+		 * Overlayable sound clip playing
+		 * Returns a string indicating the clip that is playing
+		 */
+		public string PlaySound(AudioClip clip, bool loop=false)
+		{
+			//Set the clip of our efxSource audio source to the clip passed in as a parameter.
+			string n = (clip.name!=null&&clip.name!="")?clip.name:""+(counter++);
+			AudioSource a = new AudioSource ();
+			if (clip == null||clip.loadState != AudioDataLoadState.Loaded)
+				return null;
+			a.clip = clip;
+			a.loop = loop;
+			a.Play ();
+			efxSource.Add(n,a);
+			return n;
+		}
+
+		public void SetBGM(AudioClip clip){
+			musicSource.clip = clip;
+			musicSource.loop = true;
+			musicSource.Play ();
+		}
+
+		/**
+		 * Randomly select one out of an array of audio clips to play
+		 * 
+		 */
+		public string RandomizeSfx (params AudioClip[] clips)
+		{
+			//Generate a random number between 0 and the length of our array of clips passed in.
+			int randomIndex = Random.Range(0, clips.Length);
+
+			//Set the clip to the clip at our randomly chosen index.
+			return PlaySound(clips[randomIndex]);
+		}
+
+		/**
+		 * Stops audio if it's playing
+		 * If it's not playing, returns false
+		 */
+		public bool StopAudioIfPlaying(string clip){
+			if (efxSource.ContainsKey (clip)) {
+				efxSource [clip].Stop ();
+				efxSource.Remove (clip);
+				return true;
+			}
+			return false;
+		}
+	}
 }
