57279eaa6c0acf540e2799c9ed7f200c97ac07be
SCOOBY-SURFACE\andre <andrew-lyall@hotmail.com>
2017-10-21 17:11:44 +1300

    Working notes; like complete; 100% done

--- BadManor/Assets/Scripts/UIs/Notes.cs
+++ BadManor/Assets/Scripts/UIs/Notes.cs
@@ -17,11 +17,7 @@ public class Notes : MonoBehaviour {
 
 	public void AddNote(TimeSpan time, string note) {
 		notes.Add (new string[] { time.ToString (), note });
-		//contentPos.sizeDelta = new Vector2 (contentPos.sizeDelta.x, contentPos.sizeDelta.y + NOTE_HEIGHT);
-		foreach (RectTransform rec in viewPort.GetComponentsInChildren<RectTransform>()) {
-			//rec.sizeDelta = new Vector2 (rec.sizeDelta.x, rec.sizeDelta.y - NOTE_HEIGHT);
-		}
-		//ScaleContent ();
+		ScaleContent ();
 
 		GameObject noteObj;
 		if (notes.Count % 2 == 0) {
@@ -37,11 +33,7 @@ public class Notes : MonoBehaviour {
 		noteObj.transform.SetParent (viewPort.transform);
 		noteObj.SetActive (true);
 
-	
 		noteObj.transform.position = new Vector3(292.5f, 313.5f - ((notes.Count - 1) * 85f)	, 0f);
-
-
-
 	}
 
 	void ScaleContent() {
@@ -61,12 +53,6 @@ public class Notes : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		contentPos = viewPort.GetComponent<RectTransform> ();
-		//contentPos.sizeDelta = new Vector2 (contentPos.sizeDelta.x, 0f);
-
-		AddNote (new TimeSpan (19, 20, 35), "This is my test note!");
-		AddNote (new TimeSpan (20, 03, 47), "Note two, note two, this is fucking note two!");
-		AddNote (new TimeSpan (20, 50, 12), "More notes");
-		AddNote (new TimeSpan (21, 30, 00), "This is be the latest one");
-
+		contentPos.sizeDelta = new Vector2 (contentPos.sizeDelta.x, 0f);
 	}
 }
