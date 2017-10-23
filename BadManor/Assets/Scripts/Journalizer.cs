ca4246991498617da1e25f28137454887248372f
Terran <terrankroft@gmail.com>
2017-10-24 05:25:22 +1300

    can win or lose

--- BadManor/Assets/Scripts/Journalizer.cs
+++ BadManor/Assets/Scripts/Journalizer.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections.Generic;
+using UnityEngine.SceneManagement;
 
 public class Journalizer : MonoBehaviour {
 
@@ -53,6 +54,16 @@ public class Journalizer : MonoBehaviour {
     public void newItemGet()
     {
 
+    }
+
+    public void winGame()
+    {
+        SceneManager.LoadScene("EndScreenWin");
+    }
+
+    public void loseGame()
+    {
+        SceneManager.LoadScene("EndScreenLose");
     }
 
 
