c8c1cec3746b313de5bce15739010652734791f6
mohan-cao <mohancao@yahoo.com.au>
2017-10-25 23:30:12 +1300

    SaveGame docs - need to be checked

--- BadManor/Assets/Scripts/SaveGame.cs
+++ BadManor/Assets/Scripts/SaveGame.cs
@@ -2,6 +2,9 @@
 
 namespace Assets.Scripts
 {
+    /// <summary>
+    /// Persistable class representing the Game's saved data.
+    /// </summary>
     [Serializable()]
     public class SaveGame
     {
@@ -14,6 +17,15 @@ namespace Assets.Scripts
         public int RNG;
         public ScoreManager ScoreManager;
         
+        /// <summary>
+        /// SaveGame constructor which saves current game state.
+        /// </summary>
+        /// <param name="isPluggedIn"></param>
+        /// <param name="isBelt"></param>
+        /// <param name="isMachine"></param>
+        /// <param name="currentState"></param>
+        /// <param name="rng"></param>
+        /// <param name="scoreManager"></param>
         public SaveGame(bool isPluggedIn, bool isBelt, bool isMachine, string currentState, int rng, ScoreManager scoreManager)
         {
             IsPluggedIn = isPluggedIn;
