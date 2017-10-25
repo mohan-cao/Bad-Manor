12c34bc10745fc35f76b33a49c23cfbd1c469e3b
mohan-cao <mohancao@yahoo.com.au>
2017-10-25 23:49:26 +1300

    Collectable docs - need to be checked

--- BadManor/Assets/Scripts/Items/Collectable.cs
+++ BadManor/Assets/Scripts/Items/Collectable.cs
@@ -6,7 +6,10 @@ using System.Text;
 namespace Assets.Scripts.Items
 {
     /// <summary>
-    /// Not implemented for prototype.</summary>
+    /// No longer used. Used to represent a collectable item.
+    /// Now using Fungus to represent items.
+    /// </summary>
+    [Obsolete]
     public class Collectable : Item
     {
     }
