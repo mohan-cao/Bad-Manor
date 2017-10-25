271f8a34efdb181d51569f279284a88fbf3f641e
mohan-cao <mohancao@yahoo.com.au>
2017-10-25 23:41:19 +1300

    UIManager docs - need to be checked

--- BadManor/Assets/Scripts/UIManager.cs
+++ BadManor/Assets/Scripts/UIManager.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 namespace Assets.Scripts
 {
     /// <summary>
-    /// UIManager does nothing for prototype but will eventually manage all interfaces.</summary>
+    /// UIManager does nothing for prototype but will eventually manage all interfaces.
+    /// No longer required as part of the game, however marks a point of extension for future updates.
+    /// </summary>
     [Serializable()]
     public class UIManager : MonoBehaviour
     {
