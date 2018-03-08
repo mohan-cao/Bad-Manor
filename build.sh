088baaaac7cdb6b4c0ae9ebb948d042046736117
mohan cao <mohancao@yahoo.com.au>
2018-03-09 10:55:39 +1300

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,24 +5,6 @@
 #  the project folder is "UnityProject". If this is not true then adjust the 
 #  -projectPath argument to point to the right location.
 
-## Run the editor unit tests
-echo "Running editor unit tests for ${UNITYCI_PROJECT_NAME}"
-/Applications/Unity/Unity.app/Contents/MacOS/Unity \
-	-batchmode \
-	-nographics \
-	-silent-crashes \
-	-logFile $(pwd)/unity.log \
-	-projectPath "$(pwd)/${UNITYCI_PROJECT_NAME}" \
-	-runEditorTests \
-	-editorTestsResultFile $(pwd)/test.xml \
-	-quit
-
-rc0=$?
-echo "Unit test logs"
-cat $(pwd)/test.xml
-# exit if tests failed
-if [ $rc0 -ne 0 ]; then { echo "Failed unit tests"; exit $rc0; } fi
-
 ## Make the builds
 ## Recall from install.sh that a separate module was needed for Windows build support
 echo "Attempting build of ${UNITYCI_PROJECT_NAME} for Windows"
